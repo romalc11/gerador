dfcaad82789bcc6f5011a6bd709ea5d265622125
Romário Torres Alcântara <romalc@romarios-imac.home>
2016-10-07 04:40:47 -0300

    Adding Pages

---
+++ js/index.js
@@ -0,0 +1,69 @@
+function gera(){
+    
+    var descPt = document.getElementById("descpt").value.trim();
+    var descEsp = document.getElementById("descesp").value.trim();
+    var descIng = document.getElementById("descing").value.trim();
+    var taSin = document.getElementById("tasin");
+    var taCat = document.getElementById("tacat");
+    var taRes = document.getElementById("tares");
+    
+    if (descPt === "" || descEsp === "" || descIng === ""){
+        alert("Por favor preencha todos os descritores!")
+    } else {
+        document.getElementById("tares").value = "";
+        var descritores = "";
+        var stmtFinal = "";
+
+            if (descPt == descEsp && descPt != descIng) {
+                descritores = '(mh:"' + descPt + '") OR ('+ descPt + ') OR (mh:"'+ descIng +'") OR ('+ descIng +')';
+
+            } else if (descPt == descIng && descPt != descEsp) {
+
+                descritores = '(mh:"' + descPt + '") OR ('+ descPt + ') OR (mh:"'+ descEsp +'") OR ('+ descEsp +')';
+
+            } else if (descIng == descEsp && descIng != descPt) {
+
+               descritores = '(mh:"' + descPt + '") OR ('+ descPt + ') OR (mh:"'+ descIng +'") OR ('+ descIng +')';
+
+            } else if (descPt == descEsp && descPt == descIng) {
+
+                descritores = '(mh:"' + descPt + '") OR ('+ descPt + ')';
+
+            } else {
+                descritores = '(mh:"' + descPt + '") OR ('+ descPt + ') OR (mh:"'+ descIng +'") OR ('+ descIng +') OR (mh:"' +  descEsp + '") OR ('+ descEsp +')';
+        
+            }
+
+            stmtFinal = descritores;
+
+            var sinonimos = taSin.value.split(",");
+
+            if (sinonimos.length != 0) {
+                
+                for(i = 0; i < sinonimos.length; i++){
+                    var verificada = sinonimos[i].trim();
+                    stmtFinal = stmtFinal + ' OR (' + verificada + ')';
+                }
+            }
+
+            var categorias = taCat.value.split(",");
+        
+            if (categorias.length != 0) {
+                for(i = 0; i < categorias.length; i++){
+                    var verificada = categorias[i].trim();
+                    stmtFinal = stmtFinal + ' OR (mh:' + verificada + '$)';
+                }
+            }
+
+            taRes.value = stmtFinal;
+    }
+}
+
+function limpa() {
+    document.getElementById("descpt").value = "";
+    document.getElementById("descesp").value = "";
+    document.getElementById("descing").value = "";
+    document.getElementById("tasin").value = "";
+    document.getElementById("tacat").value = "";
+    document.getElementById("tares").value = "";
+}
